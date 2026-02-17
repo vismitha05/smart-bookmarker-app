cf39bd911bf708e21c4b217434abd9b12ffe5af9
vismitha05 <vismitham31@gmail.com>
2026-02-17 17:47:47 +0530

    modified server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -12,7 +12,13 @@ export const createClient = async () => {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(
+          cookiesToSet: {
+            name: string
+            value: string
+            options?: Parameters<typeof cookieStore.set>[2]
+          }[]
+        ) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)

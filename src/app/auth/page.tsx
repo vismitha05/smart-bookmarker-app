ce6622478a0bdc87bf14d3616f9f47f260db1ef2
vismitha05 <vismitham31@gmail.com>
2026-02-17 19:26:55 +0530

    modifierd auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -30,7 +30,7 @@ export default function AuthPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3002'}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_APP_URL || 'smart-bookmarker-app-87tq.vercel.app'}/auth/callback`,
       },
     })
 
